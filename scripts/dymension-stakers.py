6dd43a492dea2aa92a312226a7621b2952f4cbf1
pops <infra@pops.one>
2024-02-23 02:57:48 +0700

    [pops] update script, add snapshot 2/23, delete 2/22

--- scripts/dymension-stakers.py
+++ scripts/dymension-stakers.py
@@ -20,19 +20,30 @@ def run_command(command):
 
 def fetch_delegations(validator_address):
     delegations = []
-    next_key = None
+    offset = 0
+    limit = 1000  # Fetch 1000 delegations at a time
+
+    # First query 1 delegator and get the total size of delegator
+    command = f"{binary} q staking delegations-to {validator_address} -o json --count-total --limit 1 --node {rpc}"
+    delegators_output = run_command(command)
+    delegators_data = json.loads(delegators_output)
+    total_delegator = int(delegators_data["pagination"].get("total"))
+    print(f"{validator_address} has {total_delegator} delegators")
+
     while True:
-        command = f"{binary} q staking delegations-to {validator_address} -o json --node {rpc}"
-        if next_key:
-            command += f" --pagination-key {next_key}"
+        command = f"{binary} q staking delegations-to {validator_address} -o json --limit {limit} --offset {offset} --node {rpc}"
         delegators_output = run_command(command)
         if not delegators_output:
             return None
         delegators_data = json.loads(delegators_output)
         delegations.extend(delegators_data["delegation_responses"])
-        next_key = delegators_data["pagination"].get("next_key")
-        if not next_key:
+        offset += limit  # Increment offset by the limit for the next chunk
+        if offset >= total_delegator:
             break
+
+    if len(delegations) != total_delegator:
+        print(f"We didn't capture all delegators. Total is {total_delegator} vs {len(delegations)}")
+        exit(1)
     return delegations
 
 # Retrieve Validators and delegators
@@ -42,17 +53,16 @@ if validators_output:
     validators_data = json.loads(validators_output)
     validator_addresses = [validator["operator_address"] for validator in validators_data["validators"]]
     for validator_address in validator_addresses:
-        delegators_output = run_command(f"{binary} q staking delegations-to {validator_address} -o json --node {rpc}")
-        if delegators_output:
-            delegators_data = json.loads(delegators_output)
-            for delegations in delegators_data["delegation_responses"]:
-                if delegator_stakes.get(delegations["delegation"]["delegator_address"]) is None:
-                    delegator_stakes[delegations["delegation"]["delegator_address"]] = 0
-                delegator_stakes[delegations["delegation"]["delegator_address"]] += atto_to_none(float(delegations["balance"]["amount"]))
-        else:
-            exit()
+        delegators_data = fetch_delegations(validator_address)
+        for delegations in delegators_data:
+            # first time adding the delegator to the dictionnary
+            if delegator_stakes.get(delegations["delegation"]["delegator_address"]) is None:
+                delegator_stakes[delegations["delegation"]["delegator_address"]] = 0
+            delegator_stakes[delegations["delegation"]["delegator_address"]] += atto_to_none(float(delegations["balance"]["amount"]))
+
 else:
-    exit()
+    print(f"Error with : {binary} q staking validators -o json --node {rpc}")
+    exit(1)
 
 # Get Latest Block
 status_output = run_command(f"{binary} status --node {rpc}")
@@ -60,15 +70,16 @@ if status_output:
     status_data = json.loads(status_output)
     snapshot_block = int(status_data["SyncInfo"]["latest_block_height"])
 else:
-    exit()
+    print("Error getting latest block")
+    exit(1)
 
 final_result = {
    "snapshot_block": snapshot_block,
    "delegators": delegator_stakes
 }
-print(json.dumps(final_result))
-
-# write file in expected format
+#print(json.dumps(final_result))
+print("write file following expected format")
+# write file following expected format
 today = datetime.now().strftime("%Y%m%d")
 with open(f"pops_{today}.csv", 'w', newline='') as file:
     for delegator in delegator_stakes:
